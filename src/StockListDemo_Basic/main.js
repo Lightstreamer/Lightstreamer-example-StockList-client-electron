5c80b71f51f3305fba391804bf988f73b662ba18
Vincenzo D'Ippolito <vincenzo.dippolito@lightstreamer.com>
2016-03-08 14:37:11 +0100

    new graphics and readme

--- src/StockListDemo_Basic/main.js
+++ src/StockListDemo_Basic/main.js
@@ -33,7 +33,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 400});
+  mainWindow = new BrowserWindow({width: 800, height: 500});
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
